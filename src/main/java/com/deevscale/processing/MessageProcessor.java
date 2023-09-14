4de6daf3d607b59a978251a8801bb0e3764ca474
dom <dom@deevscale.com>
2023-09-13 21:58:26 -0700

    Mesagging processor

--- src/main/java/com/deevscale/processing/MessageProcessor.java
+++ src/main/java/com/deevscale/processing/MessageProcessor.java
@@ -2,25 +2,8 @@ package com.deevscale.processing;
 
 public class MessageProcessor {
 
-    public int countArrays(int[] arr1, int[] arr2) {
 
-        if (arr1.length == 0 || arr2.length == 0 ) {
-            return -1;
-        }
-        int x = Integer.MAX_VALUE;
-        int indexA = 0;
-        int indexB = 0;
 
-        while (indexA < arr1.length && indexB < arr2.length) {
 
-            int val = Math.abs(arr1[indexA] - arr2[indexB]);
-            x = Math.min(x,val);
-            if (arr1[indexA] < arr2[indexB]) {
-                indexA++;
-            } else {
-                indexB++;
-            }
-        }
-        return x;
-    }
+
 }
--- src/test/java/com/deevscale/processing/MessageProcessorTest.java
+++ src/test/java/com/deevscale/processing/MessageProcessorTest.java
@@ -4,25 +4,4 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class MessageProcessorTest {
 
-    MessageProcessor principal  = new MessageProcessor();
-
-    @org.junit.jupiter.api.Test
-    void countArrays() {
-
-        int[] arr1 = {10,31,522,54,77,89,22,443,234,6,4,3234,23,28};
-        int[] arr2 = {2,3,1,2,1,2,1,2};
-
-        int actual = principal.countArrays(arr1,arr2);
-
-        System.out.println(actual);
-
-    }
-
-    @org.junit.jupiter.api.Test
-    void inter() {
-
-        System.out.println("The i is ");
-        System.out.println(35/22*2 + 33);
-
-    }
 }
